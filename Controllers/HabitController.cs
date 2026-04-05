e9e81c242672fc5b348bd34edc16a2590f8d0aea
Adam Acuña González <adam.acuna.gonzalez@est.una.ac.cr>
2026-04-04 23:29:41 -0600

    New exceptions handling in the controller, now the creation endpoint is showing a user friendly json in its response even when the result was a complete failure from the api.

--- Controllers/HabitController.cs
+++ Controllers/HabitController.cs
@@ -1,6 +1,7 @@
 using LevelUpLifeBackend.DTOs.Requests;
 using LevelUpLifeBackend.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LevelUpLifeBackend.Controllers;
 
@@ -30,7 +31,52 @@ public class HabitsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateHabitRequestDto request)
     {
-        var createdHabit = await _habitService.CreateAsync(request);
-        return CreatedAtAction(nameof(GetById), new { id = createdHabit.Id }, createdHabit);
+        try
+        {
+            var createdHabit = await _habitService.CreateAsync(request);
+
+            if (createdHabit is null)
+            {
+                return BadRequest(
+                    new
+                    {
+                        success = false,
+                        message = "No se pudo crear el hábito. Verifica los datos enviados.",
+                    }
+                );
+            }
+
+            var customResponse = new
+            {
+                success = true,
+                message = "El nuevo hábito se ha creado exitosamente.",
+            };
+            return CreatedAtAction(nameof(GetById), new { id = createdHabit.Id }, customResponse);
+        }
+        catch (DbUpdateException ex)
+        {
+            Console.WriteLine($"Error de BD: {ex.InnerException?.Message}");
+
+            return BadRequest(
+                new
+                {
+                    success = false,
+                    message = "Error guardando en la base de datos. Verifica que el Usuario y la Disciplina existan.",
+                }
+            );
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error Inesperado: {ex.Message}");
+
+            return StatusCode(
+                500,
+                new
+                {
+                    success = false,
+                    message = "Ocurrió un error inesperado al procesar tu solicitud.",
+                }
+            );
+        }
     }
 }
