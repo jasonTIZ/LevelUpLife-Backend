2a347204d2083f55b70ee613ff72cc02ecb66817
Adam Acuña <adam.acuna.gonzalez@est.una.ac.cr>
2026-04-27 23:19:16 -0600

    Controller with its respective endpoint to list the Habit Categories ready, with this the task is done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 using System.Text;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using LevelUpLifeBackend.Data;
 using LevelUpLifeBackend.Repositories;
 using LevelUpLifeBackend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,15 +12,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // Repositorios y Servicios de Hábitos
 builder.Services.AddScoped<IHabitRepository, HabitRepository>();
 builder.Services.AddScoped<IHabitService, HabitService>();
+
+// Services and Repositories of Habit Category
+builder.Services.AddScoped<IHabitCategoryRepository, HabitCategoryRepository>();
+builder.Services.AddScoped<IHabitCategoryService, HabitCategoryService>();
 
 // Repositorios y Servicios de Autenticación
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
@@ -29,18 +35,19 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 // Configuración de JWT
 // Esto le dice al framework cómo validar los tokens que llegan en los requests.
 var jwtKey = builder.Configuration["Jwt:Key"]!;
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
             ValidateAudience = true,
-            ValidateLifetime = true,           // Rechaza tokens expirados
+            ValidateLifetime = true, // Rechaza tokens expirados
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
         };
     });
 
