ebd86fd82b7c691cfc9192d916a7f29a2db9e413
Adam Acuña González <107950983+AdamAG124@users.noreply.github.com>
2026-06-19 20:36:36 -0600

    The endpoint to update an habit was updated, now it is working as it is necessary, adding a list of tasks during the edition (#94)

--- DTOs/Requests/UpdateHabitRequestDto.cs
+++ DTOs/Requests/UpdateHabitRequestDto.cs
@@ -21,13 +21,11 @@ public class UpdateHabitRequestDto
     [MaxLength(500, ErrorMessage = "La descripción no puede superar los 500 caracteres.")]
     public string Description { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Debes de agregar una disciplina a este hábito.")]
-    [Range(1, int.MaxValue, ErrorMessage = "La disciplina no es válida.")]
-    public int DisciplineId { get; set; }
-
     [Required(ErrorMessage = "Él hábito debe de estar obligatoriamente asociado a un usuario.")]
     [Range(1, int.MaxValue, ErrorMessage = "Usuario inválido.")]
     public int UserId { get; set; }
 
     public List<UpdateHabitTaskRequestDto>? Tasks { get; set; }
+
+    public List<CreateHabitTaskRequestDto>? NewTasks { get; set; }
 }
